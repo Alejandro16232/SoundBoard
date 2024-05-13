36a0c9eba87af9a2f08d60663418d8120e3cb9aa
Alejandro16232 <abel.ponce@tecsup.edu.pe>
2024-05-13 18:37:25 -0500

    Initial Commit

---
+++ SoundBoard2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SoundBoard2
+//
+//  Created by Abel Alejandro Ponce Gomez on 13/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
